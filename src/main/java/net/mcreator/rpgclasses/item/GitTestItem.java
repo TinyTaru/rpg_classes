d8b5226dfcde7e35c01a3568412534c52d1012d4
Sabien <TinyTaru@Proton.me>
2024-11-16 16:19:16 -0800

    Revert "git item update to carrot"
    This reverts commit f648c1419bb4b08c5c94ee154bff489889e2d0e5.

--- src/main/java/net/mcreator/rpgclasses/item/GitTestItem.java
+++ src/main/java/net/mcreator/rpgclasses/item/GitTestItem.java
@@ -1,26 +1,11 @@
 
 package net.mcreator.rpgclasses.item;
 
-import net.minecraft.world.level.Level;
-import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.Rarity;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
-import net.minecraft.network.chat.Component;
-
-import java.util.List;
 
 public class GitTestItem extends Item {
 	public GitTestItem() {
 		super(new Item.Properties().stacksTo(64).rarity(Rarity.COMMON));
-	}
-
-	@Override
-	public void appendHoverText(ItemStack itemstack, Level level, List<Component> list, TooltipFlag flag) {
-		super.appendHoverText(itemstack, level, list, flag);
-		list.add(Component.literal("Gah"));
-		list.add(Component.literal("boo"));
-		list.add(Component.literal("moo"));
-		list.add(Component.literal(""));
 	}
 }
